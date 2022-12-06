e0eb3b9fd4b63931d0a8ee310ec729f3aeac95e6
Surada Sri Sai <118173264+srisai3087@users.noreply.github.com>
2022-12-06 15:46:44 +0530

    Add files via upload

---
+++ python project.py
@@ -0,0 +1,16 @@
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+count_p = 0
+count_c = 0
+for i in range(num1,num2+1):
+    count = 0
+    for x in range(2, i):
+        if i % x == 0:
+            count += 1
+    if count == 0:
+        print(f"{i} is a prime number.")
+        count_p += 1
+    else:
+        print(f"{i} is a composite number.")
+        count_c += 1
+print(f"There are {count_p} prime and {count_c} composite number in the range.")
